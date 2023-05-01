d6597422e2dfe881b88382f888f4fd7a983fc3e9
C1324523 Daniela Domingues Queiros de Oliveira <c1324523@interno.bb.com.br>
2023-04-30 21:01:34 -0300

    Criando o header principal

--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -1,7 +1,23 @@
 import styled from "styled-components";
 
-export const Container = styled.div `
+export const Container = styled.div`
     grid-area: MH;
-    color: ${props => props.theme.colors.white};
-    background-color: ${props => props.theme.colors.secondary}
+    /* color: ${props => props.theme.colors.white}; */
+    background-color: ${props => props.theme.colors.secondary};
+    display: flex; //para ficar um na frente do outro
+    justify-content: space-between; //para cada um ficar de um lado, esquer e direito
+    align-items: center;
+    padding: 0 10px;
+    border-bottom: 1px solid ${props => props.theme.colors.gray};
+
+
+
 `;
+
+export const Profile = styled.div`
+    color: ${props => props.theme.colors.white};
+`;
+
+export const Welcome = styled.h3``;
+
+export const UserName = styled.span``;
